d7c37988326912217062373e11e76251dbb0eba3
朱雷 <15253480+zhu-lei722@user.noreply.gitee.com>
2025-05-28 20:29:52 +0800

    添加项目文件。

---
+++ App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using AutoUpdaterDotNET;
+using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace AutoUpdateTest
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            AutoUpdater.Start();
+        }
+    }
+
+}
